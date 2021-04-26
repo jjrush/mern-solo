eed0649722d5e0259687e279157fbb80e1487fb8
jjrush <jjrush-github@proton.me>
2021-04-25 17:29:32 -0700

    removing left over code

--- server/routes/karaoke.routes.js
+++
@@ -1,9 +0,0 @@
-const KaraokeController = require('../controllers/karaoke.controller');
-
-module.exports = (app) => {
-    app.get('/api/karaoke', KaraokeController.getAll);
-    app.post('/api/karaoke', KaraokeController.create);
-    app.get('/api/karaoke/:id', KaraokeController.getOne);
-    app.put('/api/karaoke/:id', KaraokeController.update);
-    app.delete('/api/karaoke/:id', KaraokeController.delete);
-}
